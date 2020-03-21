70722242fb5fd7ef2a27b7ef3b620b7c2882871e
Fatima-Usf <f.ucf.eva@gmail.com>
2020-03-21 16:29:43 +0100

    added client directpry

---
+++ Client/corona.js
@@ -0,0 +1,13 @@
+console.log('M service Worker');
+
+self.addEventListener('push',(event)=> {
+    console.log(' Push message ', event);
+    var title = "Corona";
+    event.waitUnitil(
+        self.registration.showNotification(title, {
+            body: 'protect urself from corona Virus',
+            tag: 'Fatima & Nada'
+            
+        }));
+
+})
---
+++ server.js
@@ -0,0 +1,35 @@
+    require('dotenv').config({ path: 'variables.env' });
+
+    const express = require('express');
+    const webPush = require('web-push');
+    const bodyParser = require('body-parser');
+    const path = require('path');
+
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    app.use(express.static(path.join(__dirname, 'client')));
+
+    const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+    const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+    webPush.setVapidDetails('mailto:test@example.com', publicVapidKey, privateVapidKey);
+
+    app.post('/subscribe', (req, res) => {
+      const subscription = req.body
+
+      res.status(201).json({});
+
+      const payload = JSON.stringify({
+        title: 'Push notifications with Service Workers',
+      });
+
+      webPush.sendNotification(subscription, payload)
+        .catch(error => console.error(error));
+    });
+
+    app.set('port', process.env.PORT || 5000);
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express running → PORT ${server.address().port}`);
+    });
